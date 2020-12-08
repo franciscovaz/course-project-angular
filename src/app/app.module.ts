7b562987038ca9242b04fd4957116b93fca20a36
Francisco Vaz <ftomasvaz@gmail.com>
2020-12-08 21:09:05 +0000

    practice directives exercice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+import { TesteComponent } from './components/teste/teste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    TesteComponent
   ],
   imports: [
     BrowserModule,
