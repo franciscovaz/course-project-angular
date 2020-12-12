59878e123bfebe8f7308009e8f83c225251e3bf0
Francisco Vaz <ftomasvaz@gmail.com>
2020-12-12 14:08:53 +0000

    delete unused code and create cockipt and server elements components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TesteComponent } from './components/teste/teste.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TesteComponent
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
