06356e8acfdbcc821f92615e77bc7ec507dc4b0a
Francisco Vaz <ftomasvaz@gmail.com>
2020-12-14 22:36:11 +0000

    binding to custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,20 @@ export class AppComponent {
       content: 'This is a test'
     }
   ];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
