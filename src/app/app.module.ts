b1b7eebc8bf5e6f5873b966bc7120467ea961094
Francisco Vaz <ftomasvaz@gmail.com>
2020-12-23 17:07:30 +0000

    building a structural directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless/unless.directive';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BetterHighlightDirective } from './better-highlight/better-highlight.di
     CockpitComponent,
     ServerElementComponent,
     BasicHighlightDirective,
-    BetterHighlightDirective
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
